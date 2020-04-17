cedbe7b7b5078fb38bf67a0902deb0eeaa38fd4f
GradyMellin <mellingrady@gmail.com>
2020-04-17 14:09:41 -0600

    commit basic javaScript and html doc

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,10 @@
+/////// Basic JavaScript 1
+// by:Grady Mellin
+//
+
+function myFirstFunc() {
+    var X = "You did a ", Y = "\"BAD\"", Z = " thing!"
+    var Y = Y.fontcolor("red")
+    document.write(X+Y+Z);
+}
+document.write(3+3)
