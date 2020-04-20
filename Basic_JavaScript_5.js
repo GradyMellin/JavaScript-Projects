34bd8439ff04dad166465f7e34f230e0ee02ba9e
GradyMellin <mellingrady@gmail.com>
2020-04-20 14:48:57 -0600

    Boolean assignment

---
+++ Basic_JavaScript_5.js
@@ -0,0 +1,32 @@
+/////// Basic JavaScript 5
+// by:Grady Mellin
+//
+
+document.write(typeof "word");
+document.write(6);
+
+function my_Function() {
+    
+    document.getElementById("test").innerHTML = isNaN('007');
+    
+}
+function not_Function() {
+    
+    document.getElementById("not").innerHTML = !(20>10);
+    
+}
+
+document.write(2E310);
+document.write(-2E310);
+document.write(3+"10");
+
+console.log(10 <= 3 && 3==3);
+console.log(10 > 3 && 3==3);
+document.write(3===3);
+document.write(3==="three");
+document.write(3==="3");
+document.write(3===4);
+
+console.log(10 <= 3 || 3==3);
+
+console.log(10 <= 3 || 3==33);
