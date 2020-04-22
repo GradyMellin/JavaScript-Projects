fcf2c63e1a7bec1fef7c57bd431e03e4d8b33bf2
GradyMellin <mellingrady@gmail.com>
2020-04-22 17:21:50 -0600

    Assingment 8 string and number methods

---
+++ Basic_JavaScript_8.js
@@ -0,0 +1,41 @@
+/////// Basic JavaScript 8
+// by:Grady Mellin
+//
+
+function sliceMethod() {
+    var sentence = "All work no play makes Grady a sad boy!";
+    var section = sentence.slice(9,39);
+    document.getElementById("Slice").innerHTML = section;
+}
+
+function upperCase() {
+    var sentence = "All work no play makes Grady a sad boy!";
+    var SENTENCE = sentence.toUpperCase();
+    document.getElementById("Upper").innerHTML = SENTENCE;
+}
+
+function search() {
+    var sentence = "All work no play makes Grady a sad boy!";
+    var position = sentence.search("Grady");
+    document.getElementById("Search").innerHTML = position;
+}
+
+function stringMethod() {
+    var X = 3242;
+    document.getElementById("N_to_S").innerHTML= X.toString();
+}
+
+function percisionMethod() {
+    var X = Math.PI;
+    document.getElementById("Percision").innerHTML= X.toPrecision(10);
+}
+
+function fixedMethod() {
+    var X = Math.PI;
+    document.getElementById("Fixed").innerHTML= X.toFixed(10);
+}
+
+function valueMethod() {
+    var X = "All work no play makes Grady a sad boy!";
+    document.getElementById("valueOf").innerHTML= X.valueOf(10);
+}
